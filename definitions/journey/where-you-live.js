f149f2ef65fad7e69f818871ad6aa107c7bb9fbe
Niall Molloy <niall.molloy@engineering.digital.dwp.gov.uk>
2020-04-22 15:45:00 +0100

    feat: add address look up for correspondence address
    Resolves AFPC-73
    
    GitOrigin-RevId: 9ce86f7a762f0858ca7351cc541211fd09a34e56

--- definitions/journey/where-you-live.js
+++ definitions/journey/where-you-live.js
@@ -1,8 +1,16 @@
 const { waypoints: WP } = require('../../lib/constants.js');
-const { d, isEqualTo, wasSkipped } = require('../../utils/journey-helpers.js');
+const {
+  d, isEqualTo, wasSkipped, isYes, isNo,
+} = require('../../utils/journey-helpers.js');
 
 module.exports = (plan) => {
-  // postcode-lookup
+  // True if claimant lives in a care home permanently
+  const inCareHome = isYes('permanentlyInCareHome', WP.CARE_HOME);
+
+  // True if claimant does not live in a care home permanently
+  const notInCareHome = isNo('permanentlyInCareHome', WP.CARE_HOME);
+
+  // Where you live postcode lookup
   // Go to address select if look up was attempted & returned array is not empty
   plan.setRoute(
     WP.WHERE_YOU_LIVE_ADDRESS_POSTCODE_LOOKUP,
@@ -40,4 +48,70 @@ module.exports = (plan) => {
 
   // Manual entry can go to next page if hidden page contains manual data
   plan.setRoute(WP.WHERE_YOU_LIVE_ADDRESS_MANUAL, WP.LETTERS_HOME, isEqualTo('addressFrom', 'manual', WP.WHERE_YOU_LIVE_ADDRESS_HIDDEN));
+
+  // Can we send letters to your home address? - if no ask for another address
+  plan.setRoute(WP.LETTERS_HOME, WP.LETTERS_ADDRESS_POSTCODE_LOOKUP, isNo('sendLettersToHome'));
+
+  // If yes continue where you live journey if not in care home otherwise skip
+  // to private pensions and begin income journey
+  plan.setRoute(WP.LETTERS_HOME, WP.LIVES_WITH_YOU, (r, c) => isYes('sendLettersToHome')(r, c) && notInCareHome(r, c));
+  plan.setRoute(WP.LETTERS_HOME, WP.PRIVATE_PENSIONS, (r, c) => isYes('sendLettersToHome')(r, c) && inCareHome(r, c));
+
+  // Correspondence postcode lookup
+  // Go to address select if look up was attempted & returned array is not empty
+  plan.setRoute(
+    WP.LETTERS_ADDRESS_POSTCODE_LOOKUP,
+    WP.LETTERS_ADDRESS_SELECT,
+    (r, c) => (
+      d(r, c).lookup_attempted && d(r, c).addresses.length > 0
+    ),
+  );
+
+  // Go to manual entry page from postcode if skip link clicked or if there was
+  // no addresses returned from address look up
+  plan.setRoute(
+    WP.LETTERS_ADDRESS_POSTCODE_LOOKUP,
+    WP.LETTERS_ADDRESS_MANUAL,
+    (r, c) => (
+      wasSkipped(WP.LETTERS_ADDRESS_POSTCODE_LOOKUP)(r, c) || (
+        d(r, c).lookup_attempted && d(r, c).addresses.length === 0
+      )
+    ),
+  );
+
+  // Select address can go to manual if 'enter manually' skip link clicked
+  plan.setRoute(
+    WP.LETTERS_ADDRESS_SELECT,
+    WP.LETTERS_ADDRESS_MANUAL,
+    wasSkipped(WP.LETTERS_ADDRESS_SELECT),
+  );
+
+  // Select address can continue to next page if hidden page contains data
+  // from select page and the page was not skipped
+  // Continue where you live journey if not in care home
+  plan.setRoute(WP.LETTERS_ADDRESS_SELECT, WP.LIVES_WITH_YOU, (r, c) => (
+    isEqualTo('addressFrom', 'select', WP.LETTERS_ADDRESS_HIDDEN)(r, c)
+    && !wasSkipped(WP.LETTERS_ADDRESS_SELECT)(r, c)
+    && notInCareHome(r, c)
+  ));
+
+  // Otherwise skip to private pensions
+  plan.setRoute(WP.LETTERS_ADDRESS_SELECT, WP.PRIVATE_PENSIONS, (r, c) => (
+    isEqualTo('addressFrom', 'select', WP.LETTERS_ADDRESS_HIDDEN)(r, c)
+    && !wasSkipped(WP.LETTERS_ADDRESS_SELECT)(r, c)
+    && inCareHome(r, c)
+  ));
+
+  // Manual entry can go to next page if hidden page contains manual data
+  // Continue where you live journey if not in care home
+  plan.setRoute(WP.LETTERS_ADDRESS_MANUAL, WP.LIVES_WITH_YOU, (r, c) => (
+    isEqualTo('addressFrom', 'manual', WP.LETTERS_ADDRESS_HIDDEN)(r, c)
+    && notInCareHome(r, c)
+  ));
+
+  // Otherwise skip to private pensions
+  plan.setRoute(WP.LETTERS_ADDRESS_MANUAL, WP.PRIVATE_PENSIONS, (r, c) => (
+    isEqualTo('addressFrom', 'manual', WP.LETTERS_ADDRESS_HIDDEN)(r, c)
+    && inCareHome(r, c)
+  ));
 };
--- definitions/pages/where-you-live.js
+++ definitions/pages/where-you-live.js
@@ -9,6 +9,7 @@ const selectAddressHooks = require('../hooks/common/select-address.js');
 const postcodeValidation = require('../field-validators/common/postcode.js');
 const selectAddressValidation = require('../field-validators/common/select-address.js');
 const manualAddressValidation = require('../field-validators/common/manual-address.js');
+const lettersHomeValidation = require('../field-validators/where-you-live/letters-home.js');
 
 module.exports = (addressServiceFactory, mountUrl) => {
   const pages = Object.create(null);
@@ -42,6 +43,45 @@ module.exports = (addressServiceFactory, mountUrl) => {
       waypoints.WHERE_YOU_LIVE_ADDRESS_POSTCODE_LOOKUP,
       waypoints.WHERE_YOU_LIVE_ADDRESS_HIDDEN,
       waypoints.WHERE_YOU_LIVE_ADDRESS_MANUAL,
+    ),
+  };
+
+  pages[waypoints.LETTERS_HOME] = {
+    view: 'pages/where-you-live/letters-home.njk',
+    fieldValidators: lettersHomeValidation,
+  };
+
+  pages[waypoints.LETTERS_ADDRESS_POSTCODE_LOOKUP] = {
+    view: 'pages/common/postcode.njk',
+    fieldValidators: postcodeValidation,
+    fieldGatherModifiers: {
+      postcode: trimWhitespace,
+    },
+    hooks: postcodeHooks(addressServiceFactory, waypoints.LETTERS_ADDRESS_MANUAL, 'lettersPageTitle'),
+  };
+
+  pages[waypoints.LETTERS_ADDRESS_SELECT] = {
+    view: 'pages/common/select-address.njk',
+    fieldValidators: selectAddressValidation,
+    hooks: selectAddressHooks(
+      waypoints.LETTERS_ADDRESS_POSTCODE_LOOKUP,
+      waypoints.LETTERS_ADDRESS_MANUAL,
+      waypoints.LETTERS_ADDRESS_HIDDEN,
+      waypoints.LETTERS_ADDRESS_SELECT,
+      'lettersPageTitle',
+    ),
+  };
+
+  pages[waypoints.LETTERS_ADDRESS_MANUAL] = {
+    view: 'pages/common/manual-address.njk',
+    fieldValidators: manualAddressValidation,
+    hooks: manualAddressHooks(
+      mountUrl,
+      '',
+      waypoints.LETTERS_ADDRESS_POSTCODE_LOOKUP,
+      waypoints.LETTERS_ADDRESS_HIDDEN,
+      waypoints.LETTERS_ADDRESS_MANUAL,
+      'lettersPageTitle',
     ),
   };
 
