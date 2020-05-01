244e5ba76d66ed8cfd863ffbbcffbdf2eb7a0ce7
Niall Molloy <niall.molloy@engineering.digital.dwp.gov.uk>
2020-05-01 10:07:48 +0100

    fix: don't let user miss moneyBackdated if changing claim dates from CYA
    GitOrigin-RevId: 27a5dd8c3a47a0d948d90c468430c8f0a880fa42

--- definitions/journey/money.js
+++ definitions/journey/money.js
@@ -1,11 +1,21 @@
 const { waypoints: WP } = require('../../lib/constants.js');
 const { citizenNeedsHRT, onlyPartnerNeedsHRT, noHRTNeeded } = require('../route-conditions/hrt.js');
+const { d } = require('../../utils/journey-helpers');
+const needToBackDate = require('../../utils/need-to-backdate');
 const over10k = require('../route-conditions/over-10k.js');
 
 module.exports = (plan) => {
   const underOrAt10k = (r, c) => !over10k(r, c);
 
-  plan.addSequence(WP.MONEY_YOU_HAVE, WP.SECOND_PROPERTY);
+  plan.setRoute(WP.MONEY_YOU_HAVE, WP.SECOND_PROPERTY, (r, c) => {
+    // If the user changes their date of claim from check your answers enabling
+    // backdating, make sure they stop at this page to complete the
+    // 'moneyBackdated' field
+    if (needToBackDate(c)) {
+      return !!d(r, c).moneyBackdated;
+    }
+    return true;
+  });
 
   // Ask about disregarded money if claimant has over £10,000 or second property
   plan.setRoute(WP.SECOND_PROPERTY, WP.DISREGARDED_MONEY, over10k);
