82c5848acbad98be5765bde07a133c0003e45d7f
james.gauld <james.gauld@engineering.digital.dwp.gov.uk>
2020-05-13 13:33:34 +0100

    chore: update dependencies
    GitOrigin-RevId: dd9a4ea7c18cceecb8c74eb7b41302794e7831cf

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-  "root": true,
-  "parser": "babel-eslint",
-  "extends": ["@dwp/eslint-config-base", "plugin:sonarjs/recommended"],
-  "plugins": [
-    "sonarjs"
+  root: true,
+  parser: 'babel-eslint',
+  extends: ['@dwp/eslint-config-base', 'plugin:sonarjs/recommended'],
+  plugins: [
+    'sonarjs',
   ],
-  "rules": {
-    "sonarjs/cognitive-complexity": [1, 10]
+  rules: {
+    'sonarjs/cognitive-complexity': [1, 10],
   },
-  "parserOptions": {
-    "ecmaVersion": "2018"
-  }
+  parserOptions: {
+    ecmaVersion: '2018',
+  },
 };
