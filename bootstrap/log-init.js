1b1f332c274dce69d4d5511cc61a2051663e66b7
james.gauld <james.gauld@engineering.digital.dwp.gov.uk>
2020-08-14 12:29:10 +0100

    refactor: reduce valid character set in trace header
    Resolves AFPC-574
    
    GitOrigin-RevId: 85fba1c41d6e9a020f97e8536afccd959e4404be

--- bootstrap/log-init.js
+++ bootstrap/log-init.js
@@ -1,6 +1,6 @@
 const { v4: uuidv4 } = require('uuid');
 
-const sanitise = (str) => String(str).replace(/[^a-z0-9;=._%/: ?()-]+/ig, '.').substr(0, 256);
+const sanitise = (str) => String(str).replace(/[^a-z0-9;=/-]+/ig, ' ').substr(0, 256);
 
 module.exports = function (baseLogger, traceRequestheaderName, sessionCookieName) {
   return (req, res, next) => {
