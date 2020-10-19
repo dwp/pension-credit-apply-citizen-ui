bbad71baa47858c4ab287bd6310832eeae84db3d
Niall Molloy <niall.molloy@engineering.digital.dwp.gov.uk>
2020-10-19 12:03:57 +0100

    fix: send an empty SVG response for header crown to work around IE issue
    GitOrigin-RevId: 5d0eee19a0e2e778f583e7e6caaaaff2b3a6905a

--- middleware/media.js
+++ middleware/media.js
@@ -17,4 +17,18 @@ module.exports = (app, mountUrl, staticDir, aggresivelyCache) => {
 
   // Send simple 404 response for missing assets
   app.use([`${mountUrl}css/`, `${mountUrl}js/`], (req, res) => res.status(404).send('Not found'));
+
+  // This sends an empty SVG for use the page header crown SVG image tag.
+  // By default the image has a empty string as its href, which prevents
+  // browsers from requesting it, but IE11 will request '/' when opening the
+  // print diaglog. The resulting start page response will initiate a new
+  // session wiping the current session on the client.
+  app.use(`${mountUrl}empty.svg`, (req, res) => {
+    // Do not re-request this on every page.
+    res.setHeader('Cache-Control', 'public, max-age=31536000, immutable');
+
+    // Must be a valid SVG or Chrome will render a broken image icon
+    res.setHeader('Content-Type', 'image/svg+xml');
+    res.status(200).send('<svg xmlns="http://www.w3.org/2000/svg"></svg>');
+  });
 };
